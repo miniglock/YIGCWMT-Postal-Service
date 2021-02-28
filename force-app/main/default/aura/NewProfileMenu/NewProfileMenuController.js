32593dd8b7e89c7e6b1b6c397af7b5d5fe0b46ec
John Carrigan <JCarrigan414@gmail.com>
2021-02-28 15:31:43 -0500

    John, Profile Menu and Contact info page

---
+++ force-app/main/default/aura/NewProfileMenu/NewProfileMenuController.js
@@ -0,0 +1,17 @@
+({
+    handleClick : function(component, event, helper) {
+        var source = event.getSource();
+        var label = source.get("v.label");
+        console.log('label'+label+'END1');
+        var urlEvent = $A.get("e.force:navigateToURL");
+        console.log('label'+label+'END2');
+        if(label=="My Account Info"){
+            urlEvent.setParams({"url":"/view-or-update-account"});
+            urlEvent.fire();
+        }else if(label=="Home"){
+            console.log('label'+label+'END3');
+            urlEvent.setParams({"url":"/ "});
+            urlEvent.fire();
+        }
+    }
+})
